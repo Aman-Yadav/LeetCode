9431b7deea29b0071d905e3462be5072faab7af1
Aman-Yadav <46748149+Aman-Yadav@users.noreply.github.com>
2021-08-22 15:32:05 +0530

    Time: 4 ms (80.00%), Space: 9.9 MB (92.00%) - LeetHub

---
+++ find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans = "";
+        for(int i=0;i<nums.size();++i){
+            if(nums[i][i] == '0')
+                ans += '1';
+            else
+                ans += '0';
+        }
+        return ans;
+    }
+};
+
+
+// class Solution {
+// public:
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         string ans="";
+//         for(int i=0; i<nums.size(); i++) 
+//             ans+= nums[i][i]=='0' ? '1' : '0';      
+//         return ans;
+//     }
+// };
